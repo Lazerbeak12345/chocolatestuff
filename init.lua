ca7c35ddcf10074ea399c54739112f2b2a02339f
Nathan Fritzler <nfblaster@live.com>
2022-01-13 20:09:52 -0700

    Use ediblestuff 1.1 api

--- init.lua
+++ init.lua
@@ -17,47 +17,22 @@ if farming.mod == "redo" then
 		"farming:chocolate_block"
 	)
 	local scale=2.5
-	ediblestuff.make_things_edible(mod,name,scale,{
-		pick=3,
-		shovel=1,
-		axe=3,
-		sword=2,
-		hoe=2 -- hoe comes with the chocolate, so we know we have it.
-	})
+	ediblestuff.make_tools_edible(mod,name,scale)
 	if minetest.get_modpath("3d_armor") ~= nil then
-		ediblestuff.make_things_edible(mod,name,scale,{
-			helmet=5,
-			chestplate=8,
-			leggings=7,
-			boots=4,
-		})
+		ediblestuff.make_armor_edible_while_wearing(mod,name,scale)
 		-- Ok, so apparently this idea for chocolate armor wasn't super original. May as well play nice.
-		local moarmour = minetest.get_modpath("moarmour")
-		for _,elm in pairs({
-			"helmet",
-			"chestplate",
-			"leggings",
-			"boots"
-		}) do
-			local elm_name=mod..":"..elm.."_"..name
-			-- Seperatly register armor as one that you can eat while wearing
-			ediblestuff.edible_while_wearing[elm_name] = true
-			if moarmour then
-				local moarmour_name = "moarmour:"..elm.."_chocolate"
-				minetest.register_alias_force(elm_name,moarmour_name)
-				ediblestuff.make_thing_edible(moarmour_name,ediblestuff.satiates[elm_name])
-				ediblestuff.edible_while_wearing[moarmour_name] = true
+		if minetest.get_modpath("moarmour") then
+			ediblestuff.make_armor_edible_while_wearing("moarmour",name,scale)
+			for _,elm in pairs({
+				"helmet",
+				"chestplate",
+				"leggings",
+				"boots"
+			}) do
+				minetest.register_alias_force(mod..":"..elm.."_"..name,"moarmour:"..elm.."_chocolate")
 			end
-		end
-		if minetest.get_modpath("shields") ~= nil then
-			local shield_item_name = mod..":shield_"..name
-			ediblestuff.make_thing_edible(shield_item_name,scale*7)
-			ediblestuff.edible_while_wearing[shield_item_name] = true
-			if moarmour then
-				local moarmour_name = "moarmour:shield_chocolate"
-				minetest.register_alias_force(shield_item_name,moarmour_name)
-				ediblestuff.make_thing_edible(moarmour_name,ediblestuff.satiates[shield_item_name])
-				ediblestuff.edible_while_wearing[moarmour_name] = true
+			if minetest.get_modpath("shields") ~= nil then
+				minetest.register_alias_force(mod..":shield_"..name,"moarmour:shield_chocolate")
 			end
 		end
 	end
