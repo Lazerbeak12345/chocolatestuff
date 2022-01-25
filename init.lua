6fe153decd75b6a73fb63bac042ca8e468b90673
Nathan Fritzler <nfblaster@live.com>
2022-01-25 10:04:26 -0700

    Reorder some messy code to clean up a bit.

--- init.lua
+++ init.lua
@@ -1,8 +1,31 @@
-local use_farming_redo = minetest.get_modpath("farming") ~= nil and farming.mod == "redo"
-local use_mtfoods = minetest.get_modpath("mtfoods") ~= nil and not use_farming_redo
-local use_food = minetest.get_modpath("food_basic") ~= nil and not use_mtfoods
-local use_mcl_morefood = minetest.get_modpath("mcl_morefood") ~= nil and not use_food
-if not (use_farming_redo or use_food or use_mtfoods or use_mcl_morefood) then
+local scale=2.5
+if minetest.get_modpath("farming") ~= nil and farming.mod == "redo" then
+	minetest.register_alias_force( -- Dark chocolate sortof looks like an ingot. This is why this mod is even a thing.
+		"chocolatestuff:chocolate_ingot",
+		"farming:chocolate_dark"
+	)
+	minetest.register_alias_force(
+		"chocolatestuff:chocolateblock",
+		"farming:chocolate_block"
+	)
+elseif minetest.get_modpath("mtfoods") ~= nil then
+	scale=1.5 -- mtfoods:chocolate doesn't satiate as much as farming:chocolate_dark
+	minetest.register_alias_force(
+		"chocolatestuff:chocolate_ingot",
+		"mtfoods:chocolate"
+	)
+elseif minetest.get_modpath("food_basic") ~= nil then
+	minetest.register_alias_force(
+		"chocolatestuff:chocolate_ingot",
+		"foods:dark_chocolate"
+	)
+elseif minetest.get_modpath("mcl_morefood") ~= nil then
+	scale=5.5  -- Chocolate in this mod is very filling
+	minetest.register_alias_force(
+		"chocolatestuff:chocolate_ingot",
+		"mcl_morefood:chocolate_dark"
+	)
+else
 	minetest.log("info","chocolatestuff: could not find source for chocolate. Chocolate armor will not be obtainable in survival.")
 end
 instant_ores.register_metal({ -- cuz eating your armor is so metal
@@ -12,36 +35,6 @@ instant_ores.register_metal({ -- cuz eating your armor is so metal
 	power = .5, -- So weak as to crumble after but a few real uses.
 	color = "#653302",  -- Color sampled from the chocolate color in the farming redo mod
 })
-local scale
-if use_farming_redo then
-	scale=2.5
-	minetest.register_alias_force( -- Dark chocolate sortof looks like an ingot. This is why this mod is even a thing.
-		"chocolatestuff:chocolate_ingot",
-		"farming:chocolate_dark"
-	)
-	minetest.register_alias_force(
-		"chocolatestuff:chocolateblock",
-		"farming:chocolate_block"
-	)
-elseif use_mtfoods then
-	scale=1.5 -- mtfoods:chocolate doesn't satiate as much as farming:chocolate_dark
-	minetest.register_alias_force(
-		"chocolatestuff:chocolate_ingot",
-		"mtfoods:chocolate"
-	)
-elseif use_food then
-	scale=2.5
-	minetest.register_alias_force(
-		"chocolatestuff:chocolate_ingot",
-		"foods:dark_chocolate"
-	)
-elseif use_mcl_morefood then
-	scale=5.5  -- Chocolate in this mod is very filling
-	minetest.register_alias_force(
-		"chocolatestuff:chocolate_ingot",
-		"mcl_morefood:chocolate_dark"
-	)
-end
 ediblestuff.make_tools_edible("chocolatestuff","chocolate",scale)
 if minetest.get_modpath("3d_armor") == nil then return end
 ediblestuff.make_armor_edible_while_wearing("chocolatestuff","chocolate",scale)
