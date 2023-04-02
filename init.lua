e0c919a169fb0661958166eb27d44cd7e7b85d15
Nathan Fritzler <nfblaster@live.com>
2023-04-02 13:31:16 -0600

    fix(aliases): Minetest broke my aliases
    Need to be at end.

--- init.lua
+++ init.lua
@@ -1,4 +1,34 @@
 local scale=2.5
+instant_ores.register_metal({ -- cuz eating your armor is so metal
+	name = "chocolatestuff:chocolate",
+	description = "Chocolate",
+	artificial = true,  -- We don't need ores.
+	power = .5, -- So weak as to crumble after but a few real uses.
+	color = "#653302",  -- Color sampled from the chocolate color in the farming redo mod
+})
+ediblestuff.make_tools_edible("chocolatestuff","chocolate",scale)
+if minetest.get_modpath("3d_armor") == nil then return end
+local armor_types=ediblestuff.make_armor_edible_while_wearing("chocolatestuff","chocolate",scale)
+-- Ok, so apparently this idea for chocolate armor wasn't super original. May as well play nice.
+local made_aliases = false
+for _,armormod in ipairs{"moarmour","armor_addon"} do
+	if minetest.get_modpath(armormod) then
+		ediblestuff.make_armor_edible_while_wearing(armormod,"chocolate",scale)
+		if made_aliases then break end
+		made_aliases = true
+		for typ,_ in pairs(armor_types) do
+			minetest.register_alias_force("chocolatestuff:"..typ.."_chocolate",    armormod..":"..typ.."_chocolate")
+		end
+	end
+end
+-- If neither of the other mods are present...
+if not made_aliases then
+	for typ,_ in pairs(armor_types) do
+		minetest.register_alias_force("moarmour:"..typ.."_chocolate",       "chocolatestuff:"..typ.."_chocolate")
+		minetest.register_alias_force("armor_addon:"..typ.."_chocolate",    "chocolatestuff:"..typ.."_chocolate")
+	end
+end
+-- Aliases apparently need to be _after_ everything else now. Unkown which version of minetest broke this.
 if minetest.get_modpath("farming") ~= nil and farming.mod == "redo" then
 	minetest.register_alias_force( -- Dark chocolate sortof looks like an ingot. This is why this mod is even a thing.
 		"chocolatestuff:chocolate_ingot",
@@ -32,33 +62,4 @@ elseif minetest.get_modpath("mcl_morefood") ~= nil then
 	)
 else
 	minetest.log("info","chocolatestuff: could not find source for chocolate. Chocolate armor will not be obtainable in survival.")
-end
-instant_ores.register_metal({ -- cuz eating your armor is so metal
-	name = "chocolatestuff:chocolate",
-	description = "Chocolate",
-	artificial = true,  -- We don't need ores.
-	power = .5, -- So weak as to crumble after but a few real uses.
-	color = "#653302",  -- Color sampled from the chocolate color in the farming redo mod
-})
-ediblestuff.make_tools_edible("chocolatestuff","chocolate",scale)
-if minetest.get_modpath("3d_armor") == nil then return end
-local armor_types=ediblestuff.make_armor_edible_while_wearing("chocolatestuff","chocolate",scale)
--- Ok, so apparently this idea for chocolate armor wasn't super original. May as well play nice.
-local made_aliases = false
-for _,armormod in ipairs{"moarmour","armor_addon"} do
-	if minetest.get_modpath(armormod) then
-		ediblestuff.make_armor_edible_while_wearing(armormod,"chocolate",scale)
-		if made_aliases then break end
-		made_aliases = true
-		for typ,_ in pairs(armor_types) do
-			minetest.register_alias_force("chocolatestuff:"..typ.."_chocolate",    armormod..":"..typ.."_chocolate")
-		end
-	end
-end
--- If neither of the other mods are present...
-if not made_aliases then
-	for typ,_ in pairs(armor_types) do
-		minetest.register_alias_force("moarmour:"..typ.."_chocolate",       "chocolatestuff:"..typ.."_chocolate")
-		minetest.register_alias_force("armor_addon:"..typ.."_chocolate",    "chocolatestuff:"..typ.."_chocolate")
-	end
 end
